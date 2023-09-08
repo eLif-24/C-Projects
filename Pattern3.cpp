c21be01d4f347e8c63c1803897b6c2d276d1196b
eLif-24 <kalranaman3@gmail.com>
2023-09-08 18:36:53 +0530

    Basic C

--- Pattern3.cpp
+++ Pattern3.cpp
@@ -6,12 +6,12 @@ int main()
     int A,i,j;
     printf("Enter the no of Rows");
     scanf("%i",&A);
-    /*for(i=1;i<=A;i++)
+    for(i=1;i<=A;i++)
     {        
         for(j=1;j<=i;j++)
             printf("* ");
         printf("\n");
-    }*/
+    }
     //Inverse Right angle triangle
     for(i=1;i<=A;i++)
     {
